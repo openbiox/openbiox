e7523fa68b5773ef6a98110bdfbaa2d8a09f382c
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2023-04-25 10:43:48 +0800

    update link

--- docs/js/chunk-2d21710c.2c235095.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d21710c"],{c4af:function(o){o.exports={"/home":"Home","/members":"Members","/projects":"Projects","https://community.openbiox.org":"Community","/wiki":"Wiki","/contact":"Contact Us"}}}]);
---
+++ docs/js/chunk-2d21710c.c9fd04de.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d21710c"],{c4af:function(o){o.exports={"/home":"Home","/members":"Members","/projects":"Projects","https://matrix.to/#/#openbiox:gitter.im":"Community","/wiki":"Wiki","/contact":"Contact Us"}}}]);
